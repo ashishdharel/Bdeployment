a15fb36808a6292b4b3bcb06d45a3af53d064274
Ashish Dharel <ashishdharel>
2024-07-27 11:21:49 +0545

    hi

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Secure CORS configuration
-const allowedOrigins = ['https://ashish.learn.cloudlaya.com'];
+const allowedOrigins = ['http://13.250.44.124:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
