7d3d053deb871a21e2051fe4d0d3775ed16beab1
Ashish Dharel <ashishdharel>
2024-07-30 09:32:24 +0545

    hello ji goodmorning

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // CORS configuration
 const allowedOrigins = [
   'http://3.0.50.87:3000', // For local development
-  'http://ashish.learn.cloudlaya.com:3000' // For production (change to your domain)
+  // 'http://ashish.learn.cloudlaya.com:3000' // For production (change to your domain)
 ];
 
 const corsOptions = {
